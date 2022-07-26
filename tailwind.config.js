bcc623c2d41d69a11347e250e2529f3dff803f45
Lisandra <lisandraferraz.s@hotmail.com>
2022-07-25 23:07:45 -0300

    criando sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/index.html"],
+  theme: {
+    colors: {
+      'default-green': '#1db954',
+      'dark-grey': '#212121',
+      'light-grey': '#b3b3b3',
+      'light-black': '#121212',
+      'grey': '#535353',
+      'black': '#000',
+      'white': '#fff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
